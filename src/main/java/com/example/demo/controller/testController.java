3d3e2feebc2ca13caf07376f96f0f7aa1c304b01
KwonYeonghoo <kupa10000@naver.com>
2024-05-12 11:46:37 +0900

    test controller

--- src/main/java/com/example/demo/controller/testController.java
+++ src/main/java/com/example/demo/controller/testController.java
@@ -1,5 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping("/v1/nasdaq")
+@Controller
 public class testController {
-    // test
+    
+    @GetMapping("/index")
+    public String mainPage(){
+        return "index";
+    }
 }
