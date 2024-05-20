f07b7455228842879cbcc98e7bb22e7cc46c0a39
rodiara5 <rodiara@naver.com>
2024-05-20 09:24:57 +0900

    캘린더페이지

--- src/main/resources/static/js/calendar1.js
+++ src/main/resources/static/js/calendar1.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
-        events: '../json/events.json', // JSON 파일의 경로를 지정합니다.
+        events: '/json/events.json', // JSON 파일의 경로를 지정합니다.
         locale: 'ko', // 한국어 locale 사용
         eventTimeFormat: { // 이벤트 시간 형식 설정
             hour: 'numeric',
             minute: '2-digit',
-            hour12: false ,// AM/PM 형식으로 시간 표시
+            hour12: false ,// AM/PM 형식으로 시간 표시 
         },
         dayMaxEventRows: true, // 하루에 최대 표시할 일정 행 수
         views: {
