d474ee83afd592681ccc68661d7d9578ed1e42b5
hyoungrok <aplay3@naver.com>
2024-05-21 16:11:00 +0900

    주가 예측 springboot

--- src/main/java/com/example/nasdaq/controller/mainController.java
+++ src/main/java/com/example/nasdaq/controller/mainController.java
@@ -1,18 +1,72 @@
 package com.example.nasdaq.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.example.nasdaq.model.DTO.DailyUpdateDto;
+import com.example.nasdaq.model.DTO.PredictpriceDto;
+import com.example.nasdaq.service.DailyUpdateService;
+import com.example.nasdaq.service.PredictpriceService;
 
 @RequestMapping("/v1/nasdaq")
 @Controller
 public class mainController {
+
+    @Autowired
+    private PredictpriceService predictpriceservice;
+
+    @Autowired
+    private DailyUpdateService dailyUpdateService;
+
     @GetMapping("/index")
-    public String mainPage(){
+    public String mainPage(Model model){
+        
+        List<DailyUpdateDto> positive_news = new ArrayList<>();
+        List<DailyUpdateDto> negative_news =  new ArrayList<>();
+        List<PredictpriceDto> postiive_dtos = predictpriceservice.getTop3predict_price();
+        List<PredictpriceDto> negative_dtos = predictpriceservice.getButtom3predict_price();
+        
+        for(PredictpriceDto dto:postiive_dtos){
+            DailyUpdateDto Ddto = dailyUpdateService.getOneDailyInfo(dto.getTicker(), "2024-05-20");
+            positive_news.add(Ddto);
+
+        }
+        for(PredictpriceDto dto:negative_dtos){
+            DailyUpdateDto Ddto = dailyUpdateService.getOneDailyInfo(dto.getTicker(), "2024-05-20");
+            negative_news.add(Ddto);
+        }
+        model.addAttribute("price", negative_dtos);
+        model.addAttribute("price2", postiive_dtos);
+        model.addAttribute("positive", negative_news);
+        model.addAttribute("negative", positive_news);
+
+
+
+        
         return "index";
     }
-    @GetMapping("/calendar")
-    public String move_calendar(){
-        return "calendar";
-    }
+
+    // @GetMapping("/many")
+    // public String getBookList(Model model) {
+        
+    //     List<BookDto> bookList = new ArrayList<BookDto>();
+
+    //     for(int i=1; i<=5; i++){
+    //         BookDto book = new BookDto(
+    //             (long) i, (i<3) ? true : false, "book"+i, "author"+i, (double)i*10000
+    //         );
+    //         bookList.add(book);
+    //     }
+    //     model.addAttribute("bookList", bookList);
+
+    //     return "bookList";
+    // }
+    
 }
